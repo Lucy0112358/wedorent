d68cbb24ff3bc7487da44d5de6e01649c299b7f1
Lucy0112358 <lusinehsahakian@gmail.com>
2024-10-31 16:55:12 +0400

    testing

--- RentaCar/RentaCar/obj/Debug/net8.0/RentaCar.AssemblyInfo.cs
+++ RentaCar/RentaCar/obj/Debug/net8.0/RentaCar.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentaCar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+756798e88e4ae056e10ee293a3df5a39e564d2c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc5503a84e1870f1f7b16a928096fc2229f36756")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentaCar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RentaCar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
